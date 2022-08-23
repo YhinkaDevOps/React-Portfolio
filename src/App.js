5f6587ae36e1490d531d53703bd7e47bc37ca25c
YhinkaDevOps <yinkabakare30@gmail.com>
2022-08-23 15:24:49 +0100

    changed work component title to project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Skills from "./components/Skills"
-import Work from './components/Work';
+import Project from './components/Project';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Home/>
       <About/>
       <Skills/>
-      <Work/>
+      <Project/>
     </div>
   );
 }

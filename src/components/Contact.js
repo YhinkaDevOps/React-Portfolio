ded4da435b353dc6c44c6d8197d3668a7f8f323a
YhinkaDevOps <yinkabakare30@gmail.com>
2022-08-31 04:34:14 +0100

    made adjustements to contact and installed emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import  React, { useState } from "react";
 import { BsGithub,BsLinkedin } from "react-icons/bs";
+import emailjs from '@emailjs/browser';
 
+const Result = () =>{
+  return (
+    <p className="result">Your message has been sucessfully sent!</p>
+  )
+}
 const Contact = () => {
+  const[result, setResult] = useState(false)
+ ;
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_jtwd3g8', 'template_aqon548', e.target, 'aShkbk3tf2laAsna0')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+      setResult(true)
+  };
+
+  setTimeout(()=>{
+      setResult(false)
+  },5000)
+
   return (
     <div name='contact' className= 'w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form action="https://getform.io/f/f022ce8e-bab0-46c8-9d94-ce6170e5547c"  method="POST" className='flex flex-col max-w-[600px] w-full mt-16 '>
+        <form onSubmit={sendEmail} className='flex flex-col max-w-[600px] w-full mt-12 '>
             <div className='mt-5 sm:text-center text-left' >
               <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 '>Contact Me</p>
             </div>
@@ -14,10 +39,14 @@ const Contact = () => {
               <a className='social_icon' href="https://github.com/YhinkaDevOps" target={"_blank"}><BsGithub color='white' size={20}/></a>
               <a className='social_icon' href="https://www.linkedin.com/in/olayinka-bakare-0a253815a/" target={"_blank"}><BsLinkedin color='white' size={20}/></a>
             </div>
-           <input className='bg-[#ccd6f6] p-2  ' type="text" placeholder='Name' name='name'  />
-           <input className='my-2 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email'  />
-           <textarea className='bg-[#ccd6f6] p-2' name="message" rows="9" placeholder='Message'></textarea>
-           <button className='text-white border-2  hover:bg-pink-600 hover:border-pink-600 px-10 py-2 my-2 mt-4 mx-auto flex items-center'>Submit</button>
+           <input className='bg-[#ccd6f6] my-2 p-2  ' type="text" placeholder='Name' name='name' required  />
+           <input className="bg-[#ccd6f6] p-2 " type="text" name="subject" id="" placeholder='Subject'/>
+           <input className='my-2 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' required  />
+           <textarea className='bg-[#ccd6f6] p-2' name="message" rows="8" placeholder='Message'required></textarea>
+           <button type="submit" className='text-white border-2  hover:bg-pink-600 hover:border-pink-600 px-10 py-1 my-2 mt-2 mx-auto flex items-center'>Submit</button>
+           <div className="mx-auto text-pink-600">
+              {result ? <Result/> : null}
+           </div>
         </form>
 
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,30 +6,30 @@ const Home = () => {
   return (
     <div name='home' className='bg-[#0a192f] w-full h-screen'>
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-         <div className='homebio-container  '>
-              <p className='text-pink-600 text-2xl'>I'm</p>
-              <h1 className='text-4xl md:text-5xl font-bold text-[#ccd6f6]'>
-                  Bakare <span className='name_span'>Olayinka</span>
-              </h1>
-              <h2 className='text-4xl md:text-5xl font-bold text-[#8892b0]'>
-                  I'm a Front-End Developer.
-              </h2>
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col-reverse h-full md:flex-row items-center '>
+        <div className='homebio-container flex flex-col md:mt-10 mx-auto md:w-3/5 md:ml-10 '>
+            <p className='text-pink-600 text-2xl'>I'm</p>
+            <h1 className='text-3xl sm:text-4xl md:text-5xl font-bold text-[#ccd6f6] '>
+                Bakare <span className='name_span'>Olayinka</span>
+            </h1>
+            <h2 className='text-3xl sm:text-4xl md:text-5xl font-bold text-[#8892b0]'>
+                I'm a Front-End Developer.
+            </h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>
                 I’m a front-end developer specializing in building exceptional digital experiences. Currently, I’m focused on
                 building responsive front-end web applications.
             </p>
-            <div >
-              <button className=' text-white group px-2 py-1 my-2 flex items-center hover:border-pink-600 hover:bg-pink-600 rounded-md Home-btn '>
-                <Link to='project' smooth={true} duration={500}>
-                  View Projects
-                </Link>
-              
-              </button>
-            </div>
+          <div >
+            <button className=' text-white group px-2 py-1 my-2 flex items-center hover:border-pink-600 hover:bg-pink-600 rounded-md Home-btn '>
+              <Link to='project' smooth={true} duration={500}>
+                View Projects
+              </Link>
+            
+            </button>
           </div>
-          <div className='home-img'>
-            <img src={Yinkaimg} alt="" />
+        </div>
+          <div className='home-img w-3/4 '>
+            <img  src={Yinkaimg} alt="" />
           </div>
        </div>
         
